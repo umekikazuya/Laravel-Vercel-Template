8ea8ae00029cbf97658099ff1bcd42055c89e0d8
Kazuya Umeki <108924738+umekikazuya@users.noreply.github.com>
2025-04-04 01:18:51 +0900

    Fix

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,15 @@
 <?php
 
-use Illuminate\Http\Request;
+/**
+ * @file
+ */
+
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::get('/test', function () {
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Test API endpoint',
+        'version' => '1.0.0',
+    ]);
+});
--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return response()->json([
+        'status' => 'success',
+        'message' => 'Test Web endpoint',
+        'version' => '1.0.0',
+    ]);
 });
